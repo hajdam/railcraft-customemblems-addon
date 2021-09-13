893fd7a1e173b0614a31b61986cf910f17f4b13f
hajdam <hajdam@users.sf.net>
2021-09-13 16:28:49 +0200

    Added more textures

--- README.md
+++ README.md
@@ -3,14 +3,38 @@ Custom Emblems Addon for Railcraft Mod
 
 Support for custom emblems for decorative purposes.
 
+You can put emblems into frames, on railcraft locomotives or combine with metal posts of various colors to form simple signs.
 
 Screenshot
 ----------
 
-![Screenshot](images/example_screenshot.png?raw=true)
+![Screenshot](images/example_screenshot.jpg?raw=true)
 
 Recipes
 -------
+
+Custom Engraving Bench
+
+![Custom Engraving Bench Recipe](images/custom_engraving_bench_recipe.png?raw=true)
+
+Metal Post sign with plate
+
+![Metal Post Plate Recipe](images/metal_post_plate_recipe.png?raw=true)
+
+Metal Post sign with no plate
+
+![Metal Post Sign Recipe](images/metal_post_sign_recipe.png?raw=true)
+
+Usage
+-----
+
+You can make your own emblems by editing mod file. Jar mod file is actually zip - you can rename it for editing if your archive program doesn't recognize it.
+
+Navigate to: assets/railcraft_cemblem/textures/emblems/
+
+Directory contains texture images used for custom emblems. You can add your own.
+
+custom.properties is a text file containing list of custom emblems with unique code, name to display and image to use.
 
 Building
 --------
--- src/main/java/mods/railcraft_cemblem/common/emblems/CustomItemEmblem.java
+++ src/main/java/mods/railcraft_cemblem/common/emblems/CustomItemEmblem.java
@@ -1,15 +1,23 @@
 package mods.railcraft_cemblem.common.emblems;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import java.util.List;
+import mods.railcraft.client.emblems.Emblem;
+import mods.railcraft.client.emblems.EmblemToolsClient;
 import mods.railcraft.common.core.RailcraftConfig;
-import mods.railcraft.common.emblems.ItemEmblemBase;
+import mods.railcraft.common.items.ItemRailcraft;
 import mods.railcraft.common.plugins.forge.RailcraftRegistry;
 import mods.railcraft.common.util.inventory.InvTools;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagString;
+import net.minecraft.util.EnumChatFormatting;
 
-public class CustomItemEmblem extends ItemEmblemBase {
+public class CustomItemEmblem extends ItemRailcraft {
 
     private static final String TAG = "railcraft_cemblem.custom_emblem";
     public static CustomItemEmblem item;
@@ -27,6 +35,19 @@ public class CustomItemEmblem extends ItemEmblemBase {
         NBTTagCompound nbt = InvTools.getItemData((ItemStack) stack);
         nbt.setString("emblem", identifier);
         return stack;
+    }
+
+    @SideOnly(value=Side.CLIENT)
+    public void func_77624_a(ItemStack stack, EntityPlayer player, List info, boolean adv) {
+        if (!stack.hasTagCompound()) return;
+        NBTTagCompound nbt = stack.getTagCompound();
+        NBTTagString emblemIdent = (NBTTagString)nbt.getTag("emblem");
+        if (emblemIdent == null) {
+            return;
+        }
+        Emblem emblem = EmblemToolsClient.packageManager.getEmblem(emblemIdent.func_150285_a_());
+        if (emblem == null) return;
+        info.add(EnumChatFormatting.GRAY + emblem.displayName);
     }
 
     public void func_94581_a(IIconRegister iconRegister) {
